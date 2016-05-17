307b3c273e90e3704e9a93d44bb3001dbf60bf6f
arun <arunkumarreddy.n@cognizant.com>
2016-05-17 22:43:52 +0100

    Phase 1, Gulp changes

--- build/assets/js/all.js
+++ build/assets/js/all.js
@@ -40276,43 +40276,6 @@ function getParameterByName(name, url) {
 
 
 
-myApp.directive('preCode', function($timeout){
-    return {
-      restrict: 'AE',
-      replace: true,
-      scope:{name:"@"},
-      template:"<pre class='preCode prettyprint'>sadas</pre>",
-      link:function($scope, element, attrs) {
-        $timeout(function() {
-        $('[data-behavior="sample_code"').each(function(){ 
-          var container = $(this);
-          
-          var target = container.closest('.preCode'); 
-          //if we have a target
-          
-            // get the sample's html
-            var sample_html = container.html();
-            var white_space = "☺";
-            // find how many spaces are before the part of the html
-            try {
-              white_space = sample_html.match(/\n+\s+\S/)[0].slice(0,(sample_html.match(/\n+\s+\S/)[0].length-3));
-            } catch(err) {}
-            // set up a regex to search for a white space string
-            var re = new RegExp(white_space,"g");
-            // replace white_space, < and > with &lt; and &gt; and remove the sample_code ref
-            sample_html = sample_html.replace(re,"\n").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(' data-behavior="sample_code"','');
-            // trim out any new lines at begining or end of string
-            sample_html = $.trim(sample_html);
-            // stick into target
-             element.html("<code>"+sample_html+"</code>");
-          
-        });
-        });
-      }
-
-
-      }  
-});
 myApp.directive('promoMDirective', function($compile) {
   return {
       restrict: 'AE', 
@@ -40440,6 +40403,43 @@ myApp.directive('promoMDirective', function($compile) {
   	};
 }); 
 
+myApp.directive('preCode', function($timeout){
+    return {
+      restrict: 'AE',
+      replace: true,
+      scope:{name:"@"},
+      template:"<pre class='preCode prettyprint'>sadas</pre>",
+      link:function($scope, element, attrs) {
+        $timeout(function() {
+        $('[data-behavior="sample_code"').each(function(){ 
+          var container = $(this);
+          
+          var target = container.closest('.preCode'); 
+          //if we have a target
+          
+            // get the sample's html
+            var sample_html = container.html();
+            var white_space = "☺";
+            // find how many spaces are before the part of the html
+            try {
+              white_space = sample_html.match(/\n+\s+\S/)[0].slice(0,(sample_html.match(/\n+\s+\S/)[0].length-3));
+            } catch(err) {}
+            // set up a regex to search for a white space string
+            var re = new RegExp(white_space,"g");
+            // replace white_space, < and > with &lt; and &gt; and remove the sample_code ref
+            sample_html = sample_html.replace(re,"\n").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(' data-behavior="sample_code"','');
+            // trim out any new lines at begining or end of string
+            sample_html = $.trim(sample_html);
+            // stick into target
+             element.html("<code>"+sample_html+"</code>");
+          
+        });
+        });
+      }
+
+
+      }  
+});
 myApp.directive('promoSDirective', function($compile) {
   return {
       restrict: 'AE',  
