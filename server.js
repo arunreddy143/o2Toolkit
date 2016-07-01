aafb94669bf7f018d180498a0cee095e2b0bbca5
arun <arunkumarreddy.n@cognizant.com>
2016-07-01 22:45:58 +0100

    [AN] scss files dynamic part

--- server.js
+++ server.js
@@ -24,30 +24,79 @@ if (app.get('env') === 'production') {
 app.use(express.static(path.join(__dirname, './build')));
 
 
+function readWriteDynamicFile(readWriteDynamicFile,writeFilename) {
 
-
-fs.readFile(__dirname +'/build/assets/css/promo-s/promo-s.css', "utf8", function(err, data){
-  if ( err ){ throw err;}
-  console.log("Reading file asynchronously");
-  //console.log(data);
-  writeSourcePath("<pre>"+data+"</pre>")
-});
-
-function writeSourcePath(data) {
-var writeSource = __dirname+'/dev/modules/promo-s/promo-s-css.html';
-
-fs.writeFile(writeSource, data, {"encoding":'utf8'}, function(err){
-  if ( err ) { throw err; }
-  console.log("*** File written successfully");
-  //Now reading the same file to confirm data written
-  fs.readFile(writeSource, "utf8", function(err, data){
+  fs.readFile(readWriteDynamicFile, "utf8", function(err, data){
     if ( err ){ throw err;}
-    console.log("*** Reading just written file");
+    console.log("Reading file asynchronously");
     //console.log(data);
+    writeSourcePath("<pre>"+data+"</pre>",writeFilename)
   });
 
-});
+  function writeSourcePath(data,writeFilename) {
+   var writeSource= writeFilename.replace(/\\/g,"/").replace('.scss', '-css.html') 
+  //var writeSource = __dirname+'/dev/modules/promo-s/css.html';
+
+  fs.writeFile(writeSource, data, {"encoding":'utf8'}, function(err){
+    if ( err ) { throw err; }
+    console.log("*** File written successfully");
+    //Now reading the same file to confirm data written
+    fs.readFile(writeSource, "utf8", function(err, data){
+      if ( err ){ throw err;}
+      console.log("*** Reading just written file");
+      //console.log(data);
+    });
+
+  });
+  }
+
 }
+
+var skipDirectives=['common','aboutToolkit','promo-m'];  
+function fromDir(startPath,filter){
+
+    //console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    console.log(startPath)
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+
+
+
+        if (stat.isDirectory()){
+
+            fromDir(filename,filter); //recurse
+        }
+        else if (filename.indexOf(filter)>=0) { 
+            //console.log('-- found: ',filename);
+            for(var i=0; i<skipDirectives.length; i++) {
+              //console.log(new RegExp(skipDirectives[i]).test(filename))
+                /*if(new RegExp(skipDirectives[i]).test(filename)) {
+
+                  return true
+                }*/
+                //else {
+                   readFileName=(filename.replace(/\\/g,"/").replace('.scss', '.css')).replace('dev/modules', "build/assets/css");
+
+                    readWriteDynamicFile(readFileName,filename)
+               // }
+            }
+           
+           /* console.log("old"+filename)
+            console.log("new"+readFileName)*/
+        };
+    };
+};
+fromDir( __dirname+'/dev/modules/','.scss');
+
+
 
 /*
  |--------------------------------------------------------------------------
